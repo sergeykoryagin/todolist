85fe39746583704d9b0c79e8351c03dce44f823c
sergeykoryagin <48332027+sergeykoryagin@users.noreply.github.com>
2021-04-27 21:29:17 +0300

    add auth reducer

---
+++ src/redux/authReducer.js
@@ -0,0 +1,63 @@
+import {AuthAPI} from '../api/api';
+// actions
+export const SET_AUTH_DATA = 'todo-list/auth/SET_AUTH_DATA';
+
+//init state
+const initialState = {
+  id: null,
+  email: null,
+  login: null,
+  isAuth: false
+};
+
+//reducer
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+  case SET_AUTH_DATA:
+    return {
+      id: action.id,
+      email: action.email,
+      login: action.login,
+      isAuth: action.isAuth
+    };
+  default:
+    return state;
+  }
+}
+
+//action creators
+export function setAuthUserData(id, email, login, isAuth) {
+  return {
+    type: SET_AUTH_DATA,
+    id, email, login, isAuth
+  };
+}
+
+//thunks
+export function authMe() {
+  return async dispatch => {
+    const response = await AuthAPI.me();
+    if (response.resultCode === 0) {
+      const {id, email, login} = response.data;
+      dispatch(setAuthUserData(id, email, login, true));
+    }
+  };
+}
+
+export function login(email, password, rememberMe) {
+  return async dispatch => {
+    const response = await AuthAPI.login(email, password, rememberMe);
+    if (response.resultCode === 0) {
+      dispatch(authMe());
+    }
+  };
+}
+
+export function logout() {
+  return async dispatch => {
+    const response = await AuthAPI.logout();
+    if (response.resultCode === 0) {
+      dispatch(setAuthUserData(null, null, null, false));
+    }
+  };
+}
